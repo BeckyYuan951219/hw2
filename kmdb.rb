7053acc0c4c5f1a144e8777d3ceb82e15eb4431f
BeckyYuan951219 <Becky.yuan@kellogg.northwestern.edu>
2026-02-04 04:04:27 +0000

    modified:   Gemfile.lock 	new file:   app/models/actor.rb 	new file:   app/models/agent.rb 	new file:   app/models/movie.rb 	new file:   app/models/role.rb 	new file:   app/models/studio.rb 	new file:   db/migrate/20260204033211_create_studios.rb 	new file:   db/migrate/20260204033239_create_movies.rb 	new file:   db/migrate/20260204033241_create_agents.rb 	new file:   db/migrate/20260204033242_create_actors.rb 	new file:   db/migrate/20260204033243_create_roles.rb 	modified:   kmdb.rb

--- kmdb.rb
+++ kmdb.rb
@@ -78,13 +78,191 @@
 # Delete existing data, so you'll start fresh each time this script is run.
 # Use `Model.destroy_all` code.
 # TODO!
+Role.destroy_all
+Actor.destroy_all
+Movie.destroy_all
+Studio.destroy_all
+Agent.destroy_all
+
 
 # Generate models and tables, according to the domain model.
 # TODO!
+# (done) Models + migrations were generated via rails generate, then tables created via rails db:migrate.
 
 # Insert data into the database that reflects the sample data shown above.
 # Do not use hard-coded foreign key IDs.
 # TODO!
+# Studio
+warner = Studio.new
+warner["name"] = "Warner Bros."
+warner.save
+
+# Movies
+bb = Movie.new
+bb["title"] = "Batman Begins"
+bb["year_released"] = 2005
+bb["rated"] = "PG-13"
+bb["studio_id"] = warner["id"]
+bb.save
+
+tdk = Movie.new
+tdk["title"] = "The Dark Knight"
+tdk["year_released"] = 2008
+tdk["rated"] = "PG-13"
+tdk["studio_id"] = warner["id"]
+tdk.save
+
+tdkr = Movie.new
+tdkr["title"] = "The Dark Knight Rises"
+tdkr["year_released"] = 2012
+tdkr["rated"] = "PG-13"
+tdkr["studio_id"] = warner["id"]
+tdkr.save
+
+# Actors
+christian_bale = Actor.new
+christian_bale["name"] = "Christian Bale"
+christian_bale.save
+
+michael_caine = Actor.new
+michael_caine["name"] = "Michael Caine"
+michael_caine.save
+
+liam_neeson = Actor.new
+liam_neeson["name"] = "Liam Neeson"
+liam_neeson.save
+
+katie_holmes = Actor.new
+katie_holmes["name"] = "Katie Holmes"
+katie_holmes.save
+
+gary_oldman = Actor.new
+gary_oldman["name"] = "Gary Oldman"
+gary_oldman.save
+
+heath_ledger = Actor.new
+heath_ledger["name"] = "Heath Ledger"
+heath_ledger.save
+
+aaron_eckhart = Actor.new
+aaron_eckhart["name"] = "Aaron Eckhart"
+aaron_eckhart.save
+
+maggie_gyllenhaal = Actor.new
+maggie_gyllenhaal["name"] = "Maggie Gyllenhaal"
+maggie_gyllenhaal.save
+
+tom_hardy = Actor.new
+tom_hardy["name"] = "Tom Hardy"
+tom_hardy.save
+
+joseph_gordon_levitt = Actor.new
+joseph_gordon_levitt["name"] = "Joseph Gordon-Levitt"
+joseph_gordon_levitt.save
+
+anne_hathaway = Actor.new
+anne_hathaway["name"] = "Anne Hathaway"
+anne_hathaway.save
+
+# Roles
+role = Role.new
+role["movie_id"] = bb["id"]
+role["actor_id"] = christian_bale["id"]
+role["character_name"] = "Bruce Wayne"
+role.save
+
+role = Role.new
+role["movie_id"] = bb["id"]
+role["actor_id"] = michael_caine["id"]
+role["character_name"] = "Alfred"
+role.save
+
+role = Role.new
+role["movie_id"] = bb["id"]
+role["actor_id"] = liam_neeson["id"]
+role["character_name"] = "Ra's Al Ghul"
+role.save
+
+role = Role.new
+role["movie_id"] = bb["id"]
+role["actor_id"] = katie_holmes["id"]
+role["character_name"] = "Rachel Dawes"
+role.save
+
+role = Role.new
+role["movie_id"] = bb["id"]
+role["actor_id"] = gary_oldman["id"]
+role["character_name"] = "Commissioner Gordon"
+role.save
+
+role = Role.new
+role["movie_id"] = tdk["id"]
+role["actor_id"] = christian_bale["id"]
+role["character_name"] = "Bruce Wayne"
+role.save
+
+role = Role.new
+role["movie_id"] = tdk["id"]
+role["actor_id"] = heath_ledger["id"]
+role["character_name"] = "Joker"
+role.save
+
+role = Role.new
+role["movie_id"] = tdk["id"]
+role["actor_id"] = aaron_eckhart["id"]
+role["character_name"] = "Harvey Dent"
+role.save
+
+role = Role.new
+role["movie_id"] = tdk["id"]
+role["actor_id"] = michael_caine["id"]
+role["character_name"] = "Alfred"
+role.save
+
+role = Role.new
+role["movie_id"] = tdk["id"]
+role["actor_id"] = maggie_gyllenhaal["id"]
+role["character_name"] = "Rachel Dawes"
+role.save
+
+role = Role.new
+role["movie_id"] = tdkr["id"]
+role["actor_id"] = christian_bale["id"]
+role["character_name"] = "Bruce Wayne"
+role.save
+
+role = Role.new
+role["movie_id"] = tdkr["id"]
+role["actor_id"] = gary_oldman["id"]
+role["character_name"] = "Commissioner Gordon"
+role.save
+
+role = Role.new
+role["movie_id"] = tdkr["id"]
+role["actor_id"] = tom_hardy["id"]
+role["character_name"] = "Bane"
+role.save
+
+role = Role.new
+role["movie_id"] = tdkr["id"]
+role["actor_id"] = joseph_gordon_levitt["id"]
+role["character_name"] = "John Blake"
+role.save
+
+role = Role.new
+role["movie_id"] = tdkr["id"]
+role["actor_id"] = anne_hathaway["id"]
+role["character_name"] = "Selina Kyle"
+role.save
+
+# Agent + represented actor
+ari = Agent.new
+ari["name"] = "Ari Emanuel"
+ari.save
+
+christian_bale["agent_id"] = ari["id"]
+christian_bale.save
+
 
 # Prints a header for the movies output
 puts "Movies"
@@ -93,6 +271,17 @@ puts ""
 
 # Query the movies data and loop through the results to display the movies output.
 # TODO!
+movies = Movie.joins(:studio).order(:year_released)
+
+for movie in movies
+  title = movie["title"]
+  year = movie["year_released"]
+  rated = movie["rated"]
+  studio_name = movie.studio["name"]
+
+  puts "#{title}  #{year}  #{rated}  #{studio_name}"
+end
+
 
 # Prints a header for the cast output
 puts ""
@@ -102,6 +291,16 @@ puts ""
 
 # Query the cast data and loop through the results to display the cast output for each movie.
 # TODO!
+roles = Role.joins(:movie, :actor).order("movies.year_released")
+
+for role in roles
+  movie_title = role.movie["title"]
+  actor_name = role.actor["name"]
+  character = role["character_name"]
+
+  puts "#{movie_title}  #{actor_name}  #{character}"
+end
+
 
 # Prints a header for the agent's list of represented actors output
 puts ""
@@ -111,3 +310,9 @@ puts ""
 
 # Query the actor data and loop through the results to display the agent's list of represented actors output.
 # TODO!
+ari = Agent.find_by({ "name" => "Ari Emanuel" })
+actors = Actor.where({ "agent_id" => ari["id"] })
+
+for actor in actors
+  puts actor["name"]
+end
